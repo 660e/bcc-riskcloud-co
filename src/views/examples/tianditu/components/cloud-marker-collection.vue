f9f21c0ad35d09d0938419cbd311e3040a3c7168
Min <solunar@vip.qq.com>
2024-03-06 14:23:16 +0800

    feat: 海量点位测试

---
+++ src/views/examples/tianditu/components/cloud-marker-collection.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+import { T, TLngLat } from '@bcc/utils';
+import points from '../points.json';
+
+onMounted(() => {
+  const M = new T.Map('map');
+  const lnglats: TLngLat[] = points.map((point: any) => new T.LngLat(point[0], point[1]));
+
+  M.centerAndZoom(new T.LngLat(108.95, 34.27), 6);
+  M.addOverLay(new T.CloudMarkerCollection(lnglats));
+});
+</script>
+
+<template>
+  <div id="map"></div>
+</template>
--- src/views/examples/tianditu/index.vue
+++ src/views/examples/tianditu/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="card h-full flex flex-col">
     <el-radio-group v-model="active" class="p-2.5">
-      <el-radio-button label="地图标注" value="0" />
+      <el-radio-button :value="0" label="地图标注" />
+      <el-radio-button :value="1" label="海量点位（5000+）" />
     </el-radio-group>
-    <map-annotation class="flex-1" />
+    <map-annotation v-if="active === 0" class="flex-1" />
+    <cloud-marker-collection v-if="active === 1" class="flex-1" />
   </div>
 </template>
 
@@ -11,6 +13,7 @@
 import { ref } from 'vue';
 
 import { MapAnnotation } from '@bcc/ui';
+import CloudMarkerCollection from './components/cloud-marker-collection.vue';
 
-const active = ref('0');
+const active = ref(0);
 </script>
