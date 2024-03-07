4ba1fbcd80467c3ffa1f4df356e679a34d863561
Min <solunar@vip.qq.com>
2024-03-07 12:14:49 +0800

    feat: 调整切换方式

--- src/views/examples/tianditu/index.vue
+++ src/views/examples/tianditu/index.vue
@@ -12,25 +12,33 @@ interface Annotation {
 }
 
 const active = ref(0);
+const activeChange = (value: number) => {
+  switch (value) {
+    case 0: // 地图标注
+      annotation.company.lnglat = [116.22858, 40.07734];
+      break;
+    case 1: // 海量点位
+      break;
+  }
+};
+
 const annotation = reactive<Annotation>({
   company: {}
 });
 
-onMounted(async () => {
-  // 地图标注
-  annotation.company.lnglat = [116.22858, 40.07734];
-});
+onMounted(() => activeChange(0));
 </script>
 
 <template>
   <div class="card h-full flex flex-col">
-    <el-radio-group v-model="active" class="p-2.5">
+    <el-radio-group v-model="active" @change="activeChange" class="p-2.5">
       <el-radio-button :value="0" label="地图标注" />
       <el-radio-button :value="1" label="海量点位（5000+）" />
     </el-radio-group>
+    <el-divider class="m-0" />
 
     <!-- 地图标注 -->
-    <map-annotation v-if="active === 0" :company="annotation.company" class="flex-1" />
+    <map-annotation v-if="active === 0" :company="annotation.company" class="flex-1 h-0" />
 
     <!-- 海量点位 -->
     <cloud-marker-collection v-if="active === 1" class="flex-1" />
