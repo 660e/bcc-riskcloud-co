26cd2d8fe4ae25dc1e0f44478170951fa2f54242
Min <solunar@vip.qq.com>
2024-03-11 14:00:53 +0800

    fix: 规范代码

--- src/views/examples/tianditu/components/cloud-marker-collection.vue
+++ src/views/examples/tianditu/components/cloud-marker-collection.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
 import { onMounted } from 'vue';
-import { T, TLngLat } from '@bcc/utils';
+import { T } from '@bcc/utils';
 import points from '../points.json';
 
 onMounted(() => {
   const M = new T.Map('map');
-  const lnglats: TLngLat[] = points.map((point: any) => new T.LngLat(point[0], point[1]));
+  const lnglats: any[] = points.map((point: any) => new T.LngLat(point[0], point[1]));
 
   M.centerAndZoom(new T.LngLat(108.95, 34.27), 6);
   M.addOverLay(new T.CloudMarkerCollection(lnglats));
