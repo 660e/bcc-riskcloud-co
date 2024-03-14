65913956b6a6f8d7fdeaf8c7b977f8b25be8a85b
Min <solunar@vip.qq.com>
2024-03-14 13:15:37 +0800

    feat: 初始化风险源数据

--- src/views/examples/tianditu/index.vue
+++ src/views/examples/tianditu/index.vue
@@ -1,38 +1,47 @@
 <script lang="ts" name="tianditu" setup>
-import { reactive, ref } from 'vue';
+import { ref } from 'vue';
 import { CloudMarkerCollection, MapAnnotation, SensitiveTargets } from '@bcc/ui';
 import { TDT } from '@bcc/utils';
 
-interface Targets {
-  company: {
-    lnglat: TDT.LngLat | null;
-    radius: number;
-    targets: TDT.Marker[];
-  };
+interface Risk {
+  id: number;
+  label: string;
 }
 
-const targets = reactive<Targets>({
-  company: {
-    lnglat: null,
-    radius: 0,
-    targets: []
-  }
+interface Company {
+  lnglat: TDT.LngLat | null;
+  radius?: number;
+  targets?: TDT.Marker[];
+  risks?: Risk[];
+}
+
+const company = ref<Company>({
+  lnglat: null
 });
 
 const active = ref(0);
 const activeChange = (value: number) => {
   switch (value) {
     case 1: // 周边敏感目标
-      setTimeout(() => {
-        targets.company = {
-          lnglat: [116.22858, 40.07734],
-          radius: 200,
-          targets: [
-            { id: 1, label: 'Target-001', lnglat: [116.22685, 40.07829] },
-            { id: 2, label: 'Target-002', lnglat: [116.22733, 40.07677] }
-          ]
-        };
-      }, 300);
+      company.value = {
+        lnglat: [116.22858, 40.07734],
+        radius: 200,
+        targets: [
+          { id: 1, label: 'Target-001', lnglat: [116.22685, 40.07829] },
+          { id: 2, label: 'Target-002', lnglat: [116.22733, 40.07677] }
+        ]
+      };
+      break;
+    case 2:
+      company.value = {
+        lnglat: [116.22858, 40.07734],
+        risks: [
+          { id: 1, label: 'Risk-001' },
+          { id: 2, label: 'Risk-002' },
+          { id: 3, label: 'Risk-003' },
+          { id: 4, label: 'Risk-004' }
+        ]
+      };
       break;
   }
 };
@@ -44,7 +53,7 @@ const activeChange = (value: number) => {
       <el-radio-button :value="0" label="海量点位" />
       <el-radio-button :value="1" label="周边敏感目标" />
       <el-radio-button :value="2" label="地图标注" />
-      <!-- <el-radio-button :value="3" label="地图标注" /> -->
+      <!-- <el-radio-button :value="3" label="图片标注" /> -->
       <!-- <el-radio-button :value="4" label="风险一张图" /> -->
     </el-radio-group>
     <el-divider class="m-0" />
@@ -52,8 +61,8 @@ const activeChange = (value: number) => {
     <!-- 海量点位 -->
     <cloud-marker-collection v-if="active === 0" class="flex-1" />
     <!-- 周边敏感目标 -->
-    <sensitive-targets v-if="active === 1" :company="targets.company" class="flex-1 h-0" />
+    <sensitive-targets v-if="active === 1" :company="company" class="flex-1 h-0" />
     <!-- 地图标注 -->
-    <map-annotation v-if="active === 2" class="flex-1" />
+    <map-annotation v-if="active === 2" :company="company" class="flex-1" />
   </div>
 </template>
