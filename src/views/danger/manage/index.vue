ad90301b5459469534bf70ce85e35c74a1eaf17d
Min <solunar@vip.qq.com>
2024-03-28 16:14:06 +0800

    feat: 初始化项目文件

--- src/views/danger/manage/index.vue
+++ src/views/danger/manage/index.vue
@@ -1,8 +1,4 @@
-<script lang="ts" setup>
-import { $c } from '@bcc/utils';
-
-console.log($c.encrypt(Math.random()));
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <div class="card h-full flex justify-center items-center">
