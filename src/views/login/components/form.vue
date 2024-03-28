84e83b8a65bd0a04dbcfeed2595a88408f7f50bf
Min <solunar@vip.qq.com>
2024-03-28 14:03:14 +0800

    feat: 初始化管理员账户

--- src/views/login/components/form.vue
+++ src/views/login/components/form.vue
@@ -38,8 +38,8 @@ const $keepAliveStore = useKeepAliveStore();
 
 const formsRef = ref<FormInstance>();
 const forms = reactive({
-  username: 'admin',
-  password: 'admin123',
+  username: 'co_admin',
+  password: '123456',
   code: '9999',
   uuid: ''
 });
