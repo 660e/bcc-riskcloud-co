8c19d1094a3c7df017b86a1c77b7cfcad7a47c81
Min <solunar@vip.qq.com>
2024-04-02 16:55:36 +0800

    feat: 初始化周边敏感目标页面

--- src/views/company/sensitive/index.vue
+++ src/views/company/sensitive/index.vue
@@ -1,8 +1,21 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+import { SensitiveTargets } from '@bcc/ui';
+
+const company = ref();
+
+onMounted(() => {
+  company.value = {
+    lnglat: [116.22874, 40.07758],
+    radius: 200,
+    targets: [
+      { id: 1, label: 'Target-001', lnglat: [116.22685, 40.07829] },
+      { id: 2, label: 'Target-002', lnglat: [116.22733, 40.07677] }
+    ]
+  };
+});
+</script>
 
 <template>
-  <div class="card h-full flex justify-center items-center">
-    <div></div>
-    <div>/src/views{{ $route.path }}/index.vue</div>
-  </div>
+  <sensitive-targets :company="company" class="card h-full" />
 </template>
