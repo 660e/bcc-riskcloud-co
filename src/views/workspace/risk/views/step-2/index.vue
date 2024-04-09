a3e4c482d431a610e09850be51494c1b9c837aee
Min <solunar@vip.qq.com>
2024-04-09 13:30:26 +0800

    feat: 初始化应急资源调查

--- src/views/workspace/risk/views/step-2/index.vue
+++ src/views/workspace/risk/views/step-2/index.vue
@@ -6,7 +6,7 @@ const active = ref(0);
 
 <template>
   <div class="card flex-1">
-    <el-tabs v-model="active" type="border-card" class="workspace__risk__step-2__tabs">
+    <el-tabs v-model="active" type="border-card" class="workspace__risk__step__tabs--full">
       <el-tab-pane :name="0" label="地图标注" class="h-full">地图标注</el-tab-pane>
       <el-tab-pane :name="1" label="管线标注" class="h-full">管线标注</el-tab-pane>
       <el-tab-pane :name="2" label="平面图标注" class="h-full">平面图标注</el-tab-pane>
@@ -15,14 +15,5 @@ const active = ref(0);
 </template>
 
 <style lang="scss" scoped>
-.workspace__risk__step-2__tabs {
-  border: 0;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  :deep(.el-tabs__content) {
-    flex: 1;
-    padding: 0;
-  }
-}
+@import '../index.scss';
 </style>
--- src/views/workspace/risk/views/step-3/index.vue
+++ src/views/workspace/risk/views/step-3/index.vue
@@ -1,5 +1,20 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const active = ref(0);
+</script>
 
 <template>
-  <div class="h-full flex justify-center items-center text-7xl">3</div>
+  <div class="card flex-1">
+    <el-tabs v-model="active" type="border-card" class="workspace__risk__step__tabs--full">
+      <el-tab-pane :name="0" label="应急队伍" class="h-full">应急队伍</el-tab-pane>
+      <el-tab-pane :name="1" label="应急专家" class="h-full">应急专家</el-tab-pane>
+      <el-tab-pane :name="2" label="应急装备" class="h-full">应急装备</el-tab-pane>
+      <el-tab-pane :name="3" label="应急物资" class="h-full">应急物资</el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+@import '../index.scss';
+</style>
