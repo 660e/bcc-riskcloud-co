93c7960975602f28922805f12b55021f2d29b959
Min <solunar@vip.qq.com>
2024-04-09 14:00:39 +0800

    feat: 初始化应急资源调查

--- src/views/workspace/risk/views/step-3/index.vue
+++ src/views/workspace/risk/views/step-3/index.vue
@@ -1,16 +1,21 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 
+import EmergencyTeam from './views/emergency-team.vue';
+import EmergencyExpert from './views/emergency-expert.vue';
+import EmergencyEquipment from './views/emergency-equipment.vue';
+import EmergencySupply from './views/emergency-supply.vue';
+
 const active = ref(0);
 </script>
 
 <template>
   <div class="card flex-1">
     <el-tabs v-model="active" type="border-card" class="workspace__risk__step__tabs--full">
-      <el-tab-pane :name="0" label="应急队伍" class="h-full">应急队伍</el-tab-pane>
-      <el-tab-pane :name="1" label="应急专家" class="h-full">应急专家</el-tab-pane>
-      <el-tab-pane :name="2" label="应急装备" class="h-full">应急装备</el-tab-pane>
-      <el-tab-pane :name="3" label="应急物资" class="h-full">应急物资</el-tab-pane>
+      <emergency-team :name="0" label="应急队伍" class="pro-table--no-card h-full flex" lazy />
+      <emergency-expert :name="1" label="应急专家" class="pro-table--no-card h-full flex" lazy />
+      <emergency-equipment :name="2" label="应急装备" class="pro-table--no-card h-full flex" lazy />
+      <emergency-supply :name="3" label="应急物资" class="pro-table--no-card h-full flex" lazy />
     </el-tabs>
   </div>
 </template>
---
+++ src/views/workspace/risk/views/step-3/views/emergency-expert.vue
@@ -0,0 +1,72 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { ElMessage, ElMessageBox } from 'element-plus';
+import { getDictDataType } from '@/api/modules/system';
+import { deleteDept } from '@/api/modules/company';
+import { getRiskByIndustryId } from '@/api/modules/workspace';
+import { ColumnProps } from '@/components/pro-table/interface';
+import { saveAs } from 'file-saver';
+
+import { ImportTemplateDialog } from '@bcc/components';
+import ProTable from '@/components/pro-table/index.vue';
+
+const tableRef = ref();
+const columns: ColumnProps[] = [
+  { prop: 'name', label: '队伍名称' },
+  { prop: 'type', label: '队伍类型' },
+  { prop: 'establishTime', label: '成立时间' },
+  { prop: 'address', label: '地址' },
+  { prop: 'stuffNum', label: '总人数' },
+  { prop: 'chief', label: '负责人' },
+  { prop: 'phone', label: '值班电话' },
+  { prop: 'operation', label: '操作', width: 100 }
+];
+
+const createDialogRef = ref();
+const create = (row: any = {}) => createDialogRef.value.open(row);
+
+const importTemplateDialogRef = ref();
+const importData = () => {
+  importTemplateDialogRef.value.open({
+    templateApi: getDictDataType,
+    templateName: '用户列表模板.xlsx',
+    importApi: getDictDataType
+  });
+};
+
+const exportData = async () => {
+  const blob: any = await getDictDataType({ ...tableRef.value.searchParam, ids: tableRef.value.selectedListIds });
+  saveAs(blob, `user_${new Date().getTime()}.xlsx`);
+};
+
+const remove = (row: any) => {
+  const name = row.id ? `“${row.name}”` : '';
+  const ids = row.id ? [row.id] : tableRef.value.selectedListIds;
+  ElMessageBox.confirm(`是否删除${name}？`, '系统提示', { type: 'warning' })
+    .then(async () => {
+      const { msg } = await deleteDept(ids.join(','));
+      tableRef.value.search(tableRef.value.pageable?.pageNum);
+      tableRef.value.clearSelection();
+      ElMessage.success(msg);
+    })
+    .catch(() => false);
+};
+</script>
+
+<template>
+  <el-tab-pane v-bind="$attrs">
+    <pro-table :columns="columns" :request-api="getRiskByIndustryId" ref="tableRef" row-key="id">
+      <template #tableHeader>
+        <el-button @click="create" type="primary">新增</el-button>
+        <el-button @click="importData">导入</el-button>
+        <el-button @click="exportData">导出</el-button>
+      </template>
+      <template #operation="scope">
+        <el-button @click="remove(scope.row)" type="danger" link>删除</el-button>
+      </template>
+    </pro-table>
+
+    <!-- 导入 -->
+    <import-template-dialog @confirm="tableRef.search() && tableRef.clearSelection()" ref="importTemplateDialogRef" />
+  </el-tab-pane>
+</template>
