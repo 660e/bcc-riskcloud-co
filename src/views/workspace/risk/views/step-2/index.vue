ec84afca2b13a46d02d17d0f1e1585cbf08ac954
Min <solunar@vip.qq.com>
2024-04-09 13:07:44 +0800

    feat: 初始化风险源标注

--- src/views/workspace/risk/views/step-2/index.vue
+++ src/views/workspace/risk/views/step-2/index.vue
@@ -5,12 +5,24 @@ const active = ref(0);
 </script>
 
 <template>
-  <div class="h-full flex flex-col">
-    <el-radio-group v-model="active" class="p-2.5">
-      <el-radio-button :value="0" label="地图" />
-      <el-radio-button :value="1" label="管线" />
-      <el-radio-button :value="2" label="平面图" />
-    </el-radio-group>
-    <el-divider class="m-0" />
+  <div class="card flex-1">
+    <el-tabs v-model="active" type="border-card" class="workspace__risk__step-2__tabs">
+      <el-tab-pane :name="0" label="地图标注" class="h-full">地图标注</el-tab-pane>
+      <el-tab-pane :name="1" label="管线标注" class="h-full">管线标注</el-tab-pane>
+      <el-tab-pane :name="2" label="平面图标注" class="h-full">平面图标注</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.workspace__risk__step-2__tabs {
+  border: 0;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  :deep(.el-tabs__content) {
+    flex: 1;
+    padding: 0;
+  }
+}
+</style>
