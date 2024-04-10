4d01995a5ce3b66f8e9cfe6b1709a0e140a16607
Min <solunar@vip.qq.com>
2024-04-10 10:29:31 +0800

    feat: 添加mock数据

--- src/views/information/report/index.vue
+++ src/views/information/report/index.vue
@@ -1,8 +1,42 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { getSubordinateAccount } from '@/api/modules/subordinate';
+import { ColumnProps } from '@/components/pro-table/interface';
+
+import ProTable from '@/components/pro-table/index.vue';
+
+const tableRef = ref();
+const columns: ColumnProps[] = [
+  { prop: 'placeName', label: '单位名称', search: { el: 'input' } },
+  { prop: 'countyName', label: '行政区划', search: { el: 'input' } },
+  { prop: 'secondName', label: '行业领域' },
+  { prop: 'officeName', label: '行业管理部门' },
+  { prop: 'isSmallName', label: '企业规模' },
+  { prop: 'oot', label: '数据来源' },
+  { prop: 'statusName', label: '账号审核状态' },
+  { prop: 'checkStatusName', label: '核查状态' },
+  { prop: 'checkOfficeName', label: '核查机构' },
+  { prop: 'checkPersonnelName', label: '核查人' },
+  { prop: 'checkTime', label: '核查时间' },
+  { prop: 'allSource', label: '总得分' },
+  { prop: 'operation', label: '操作', width: 200 }
+];
+
+const view = (row: any) => console.log(row);
+</script>
 
 <template>
-  <div class="card h-full flex justify-center items-center">
-    <div></div>
-    <div>/src/views{{ $route.path }}/index.vue</div>
+  <div class="h-full flex flex-col">
+    <pro-table :columns="columns" :request-api="getSubordinateAccount" ref="tableRef" row-key="id">
+      <template #tableHeader>
+        <el-button type="primary">新增</el-button>
+        <el-button>核销</el-button>
+        <el-button>关联下属公司</el-button>
+      </template>
+      <template #operation="scope">
+        <el-button @click="view(scope.row)" type="primary" link>修改</el-button>
+        <el-button @click="view(scope.row)" type="primary" link>解除上下级关系</el-button>
+      </template>
+    </pro-table>
   </div>
 </template>
