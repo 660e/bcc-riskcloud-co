183cbd5bc79609dddf9af05c440a7cb36dae9464
Min <solunar@vip.qq.com>
2024-04-11 15:08:35 +0800

    feat: 风险更新记录mock数据

--- src/api/modules/workspace.ts
+++ src/api/modules/workspace.ts
@@ -1,13 +1,22 @@
 import http from '@/api';
 
+// 获取风险更新记录
+export function getWorkspaceLog() {
+  return http.get('/mock/co/workspace/log');
+}
+
+// 获取风险统计信息
+export function getWorkspaceStatistics() {
+  return http.get('/mock/co/workspace/statistics');
+}
+
+//
+//
+//
+
 // 根据行业领域获取风险源列表
 export function getRiskByIndustryId(params: any) {
   return http.get<any>('/mock/co/workspace/risk', params);
-}
-
-// 获取风险更新记录列表
-export function getWorkspaceLog() {
-  return http.get('/mock/co/workspace/log');
 }
 
 // 获取线上核查结果列表
--- src/views/workspace/log/dialogs/statistic.vue
+++ src/views/workspace/log/dialogs/statistic.vue
@@ -1,20 +1,21 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
+import { getWorkspaceStatistics } from '@/api/modules/workspace';
 import { RiskStatistics } from '@bcc/ui';
 
 const visible = ref(false);
-
-const open = async (row: any) => {
-  console.log(row);
+const data = ref();
+const open = async () => {
   visible.value = true;
+  data.value = (await getWorkspaceStatistics()).data;
 };
 
 defineExpose({ open });
 </script>
 
 <template>
-  <el-dialog v-model="visible" title="历史风险统计" width="1200">
-    <risk-statistics />
+  <el-dialog v-model="visible" @close="data = null" title="历史风险统计" width="1200" destroy-on-close>
+    <risk-statistics :data="data" />
     <template #footer>
       <div class="flex justify-end">
         <el-button @click="visible = false">关闭</el-button>
