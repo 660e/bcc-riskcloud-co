aee1235e9a157a187d5c207d3bf528ebf5276cfe
Min <solunar@vip.qq.com>
2024-04-11 16:22:01 +0800

    feat: 账号审核核销、恢复、解除上下级关系mock数据

--- src/views/subordinate/account/index.vue
+++ src/views/subordinate/account/index.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
+import { ElMessage, ElMessageBox } from 'element-plus';
 import { getSubordinateAccount } from '@/api/modules/subordinate';
+import { deleteItem } from '@/api/modules/company';
 import { ColumnProps } from '@/components/pro-table/interface';
 import CreateDialog from './dialogs/create.vue';
 import ProTable from '@/components/pro-table/index.vue';
@@ -12,13 +14,38 @@ const columns: ColumnProps[] = [
   { prop: 'countyName', label: '所属区域', search: { el: 'input' } },
   { prop: 'parentName', label: '上级单位' },
   { prop: 'statusName', label: '状态' },
-  { prop: 'operation', label: '操作', width: 200 }
+  { prop: 'operation', label: '操作', width: 280 }
 ];
 
 const createDialogRef = ref();
 const create = (row: any = {}) => createDialogRef.value.open(row);
 
-const view = (row: any) => console.log(row);
+const handle = (row: any, cmd: 'write-off' | 'revert' | 'unbind') => {
+  let fn: any;
+
+  switch (cmd) {
+    case 'write-off':
+      fn = deleteItem;
+      break;
+    case 'revert':
+      fn = deleteItem;
+      break;
+    case 'unbind':
+      fn = deleteItem;
+      break;
+  }
+
+  const name = row.id ? `“${row.placeName}”` : '';
+  const ids = row.id ? [row.id] : tableRef.value.selectedListIds;
+  ElMessageBox.confirm(`是否解除${name}？`, '系统提示', { type: 'warning' })
+    .then(async () => {
+      const { msg } = await fn(ids.join(','));
+      tableRef.value.search(tableRef.value.pageable?.pageNum);
+      tableRef.value.clearSelection();
+      ElMessage.success(msg);
+    })
+    .catch(() => false);
+};
 </script>
 
 <template>
@@ -26,14 +53,18 @@ const view = (row: any) => console.log(row);
     <pro-table :columns="columns" :request-api="getSubordinateAccount" ref="tableRef" row-key="id">
       <template #tableHeader>
         <el-button @click="create()" type="primary">新增</el-button>
-        <el-button :disabled="!tableRef?.selectedListIds.length">恢复</el-button>
-        <el-button :disabled="!tableRef?.selectedListIds.length">核销</el-button>
-        <el-button :disabled="!tableRef?.selectedListIds.length" type="danger" plain>解除上下级关系</el-button>
+        <el-button @click="handle({}, 'write-off')" :disabled="!tableRef?.selectedListIds.length">核销</el-button>
+        <el-button @click="handle({}, 'revert')" :disabled="!tableRef?.selectedListIds.length">恢复</el-button>
         <el-button>关联下属公司</el-button>
+        <el-button @click="handle({}, 'unbind')" :disabled="!tableRef?.selectedListIds.length" type="danger" plain>
+          解除上下级关系
+        </el-button>
       </template>
       <template #operation="scope">
         <el-button @click="create(scope.row)" type="primary" link>修改</el-button>
-        <el-button @click="view(scope.row)" type="primary" link>解除上下级关系</el-button>
+        <el-button @click="handle(scope.row, 'write-off')" type="primary" link>核销</el-button>
+        <el-button @click="handle(scope.row, 'revert')" type="primary" link>恢复</el-button>
+        <el-button @click="handle(scope.row, 'unbind')" type="danger" link>解除上下级关系</el-button>
       </template>
     </pro-table>
 
