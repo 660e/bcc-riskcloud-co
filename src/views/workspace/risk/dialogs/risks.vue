68040ebe8a802e8518987a4c8a94dbb49c385ab3
Min <solunar@vip.qq.com>
2024-04-15 17:01:31 +0800

    feat: 风险源列表mock数据

--- src/views/workspace/risk/dialogs/risks.vue
+++ src/views/workspace/risk/dialogs/risks.vue
@@ -4,18 +4,17 @@ import { getRisks } from '@/api/modules/workspace';
 
 const visible = ref(false);
 const risks = ref();
+const checkList = ref([]);
 
 const open = async () => {
   visible.value = true;
-  const { data } = await getRisks();
-  console.log(data);
-  risks.value = data;
+  risks.value = (await getRisks()).data;
 };
 const close = () => {
   console.log('close');
 };
 const confirm = () => {
-  visible.value = false;
+  console.log(checkList.value);
 };
 
 open();
@@ -24,10 +23,19 @@ defineExpose({ open });
 </script>
 
 <template>
-  <el-dialog v-model="visible" @close="close" title="风险源" width="1200" align-center>
-    <el-tabs type="border-card" tab-position="left" class="_risks">
-      <el-tab-pane v-for="risk in risks" :key="risk.id" :label="risk.name">
-        <pre>{{ risk.children }}</pre>
+  <el-dialog v-model="visible" @close="close" title="风险源" width="80vw" align-center>
+    <el-tabs tab-position="left" type="border-card" class="_risks">
+      <el-tab-pane v-for="industry in risks" :key="industry.id" :label="industry.name" class="pb-5">
+        <template v-for="level in industry.children" :key="level.id">
+          <div class="bg-blue-400 pl-2.5 text-white flex items-center" style="height: 38px">{{ level.name }}</div>
+          <el-collapse>
+            <el-collapse-item v-for="group in level.children" :key="group.id" :title="group.name" class="px-2.5">
+              <el-checkbox-group v-model="checkList">
+                <el-checkbox v-for="risk in group.children" :key="risk.id" :label="risk.name" :value="risk.id" />
+              </el-checkbox-group>
+            </el-collapse-item>
+          </el-collapse>
+        </template>
       </el-tab-pane>
     </el-tabs>
     <template #footer>
