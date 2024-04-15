f1b8968bd2cc5472d343eab7c245f6e87d9d657e
Min <solunar@vip.qq.com>
2024-04-15 11:06:19 +0800

    feat: 优化步骤条展示效果

--- src/views/workspace/risk/components/steps.vue
+++ src/views/workspace/risk/components/steps.vue
@@ -13,14 +13,19 @@ defineEmits(['next', 'prev']);
         <div class="pl-1">上一步</div>
       </el-button>
     </div>
-    <div class="flex-1 flex justify-center items-center space-x-4">
+    <div class="flex-1 flex justify-center items-center">
       <div
         v-for="(e, i) in ['风险源辨识评估', '风险源标注', '应急资源调查', '应急能力评估', '生成评估信息', '签字确认']"
         :key="i"
-        :class="{ 'text-red-600': active > i }"
+        :style="{ backgroundColor: active > i ? 'var(--el-color-primary)' : '' }"
+        class="flex-1 self-stretch flex justify-center items-center space-x-2 bg-gray-200"
+        style="
+          clip-path: polygon(0 0, calc(100% - 16px) 0, 100% 50%, calc(100% - 16px) 100%, 0 100%, 16px 50%, 0 0);
+          margin-left: -10px;
+        "
       >
-        <span>{{ i + 1 }}</span>
-        <span>{{ e }}</span>
+        <span class="w-5 h-5 bg-white rounded-full flex justify-center items-center">{{ i + 1 }}</span>
+        <span :class="{ 'text-white': active > i }">{{ e }}</span>
       </div>
     </div>
     <div class="w-40 flex justify-end">
