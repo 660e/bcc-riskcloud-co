6befee2618b1b6c809f63eee21cefe11a4e48f97
Min <solunar@vip.qq.com>
2024-04-28 11:39:27 +0800

    feat: 初始化部门管理页面

--- src/views/company/organization/views/dept.vue
+++ src/views/company/organization/views/dept.vue
@@ -1,13 +1,15 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 import { ElMessage, ElMessageBox } from 'element-plus';
 import { deleteItem } from '@/api/modules/company';
 import { getEmergencyEquipment } from '@/api/modules/workspace';
 import { ColumnProps } from '@/components/pro-table/interface';
+import { TreeFilter } from '@bcc/components';
 
 import CreatePostDialog from '../dialogs/create-post.vue';
 import ProTable from '@/components/pro-table/index.vue';
 
+const treeFilterValue = reactive({ departmentId: '1' });
 const tableRef = ref();
 const columns: ColumnProps[] = [
   { type: 'selection', width: 0 },
@@ -23,6 +25,11 @@ const columns: ColumnProps[] = [
   { prop: 'phone', label: '联系电话' },
   { prop: 'operation', label: '操作', width: 100 }
 ];
+
+const changeTreeFilter = (val: string) => {
+  ElMessage.success(`你选择了 id 为 ${val} 的数据🤔`);
+  treeFilterValue.departmentId = val;
+};
 
 const createPostDialogRef = ref();
 const create = (row: any = {}) => createPostDialogRef.value.open(row);
@@ -43,17 +50,26 @@ const remove = (row: any) => {
 
 <template>
   <el-tab-pane v-bind="$attrs">
-    <pro-table :columns="columns" :request-api="getEmergencyEquipment" ref="tableRef" row-key="id">
-      <template #tableHeader>
-        <el-button @click="create" type="primary">新增</el-button>
-        <el-button :disabled="!tableRef?.selectedListIds.length" @click="remove" type="danger" plain>删除</el-button>
-      </template>
-      <template #operation="scope">
-        <el-button @click="remove(scope.row)" type="danger" link>删除</el-button>
-      </template>
-    </pro-table>
+    <div class="flex">
+      <tree-filter
+        label="name"
+        title="部门列表(单选)"
+        :request-api="getEmergencyEquipment"
+        :default-value="treeFilterValue.departmentId"
+        @change="changeTreeFilter"
+      />
+      <pro-table :columns="columns" :request-api="getEmergencyEquipment" ref="tableRef" row-key="id">
+        <template #tableHeader>
+          <el-button @click="create" type="primary">新增</el-button>
+          <el-button :disabled="!tableRef?.selectedListIds.length" @click="remove" type="danger" plain>删除</el-button>
+        </template>
+        <template #operation="scope">
+          <el-button @click="remove(scope.row)" type="danger" link>删除</el-button>
+        </template>
+      </pro-table>
 
-    <!-- 新增 -->
-    <create-post-dialog @confirm="tableRef.search() && tableRef.clearSelection()" ref="createPostDialogRef" />
+      <!-- 新增 -->
+      <create-post-dialog @confirm="tableRef.search() && tableRef.clearSelection()" ref="createPostDialogRef" />
+    </div>
   </el-tab-pane>
 </template>
