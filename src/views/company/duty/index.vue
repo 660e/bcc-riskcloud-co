6572dfb37eeae6c23f08d1549ffbf0e28dc9177e
Min <solunar@vip.qq.com>
2024-04-28 11:04:29 +0800

    chore: 迁移主体责任清单模块

--- src/views/company/duty/index.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" name="company-duty" setup></script>
-
-<template>
-  <div class="card h-full flex justify-center items-center">
-    <div>/src/views{{ $route.path }}/index.vue</div>
-  </div>
-</template>
--- src/views/list/duty/index.vue
+++ src/views/list/duty/index.vue
@@ -1,41 +1,7 @@
-<script lang="ts" setup>
-import { ref } from 'vue';
-import { getWorkspaceOnline } from '@/api/modules/workspace';
-import { ColumnProps } from '@/components/pro-table/interface';
-
-import ProTable from '@/components/pro-table/index.vue';
-import DetailDialog from './dialogs/detail.vue';
-
-const tableRef = ref();
-const columns: ColumnProps[] = [
-  { prop: 'placeName', label: '单位名称', search: { el: 'input' } },
-  { prop: 'countyName', label: '行政区划', search: { el: 'input' } },
-  { prop: 'secondName', label: '行业领域' },
-  { prop: 'officeName', label: '行业管理部门' },
-  { prop: 'isSmallName', label: '企业规模' },
-  { prop: 'oot', label: '数据来源' },
-  { prop: 'statusName', label: '账号审核状态' },
-  { prop: 'checkStatusName', label: '核查状态' },
-  { prop: 'checkOfficeName', label: '核查机构' },
-  { prop: 'checkPersonnelName', label: '核查人' },
-  { prop: 'checkTime', label: '核查时间' },
-  { prop: 'allSource', label: '总得分' },
-  { prop: 'operation', label: '操作', width: 100 }
-];
-
-const detailDialogRef = ref();
-const detail = (row: any) => detailDialogRef.value.open(row);
-</script>
+<script lang="ts" name="company-duty" setup></script>
 
 <template>
-  <div class="h-full flex flex-col">
-    <pro-table :columns="columns" :request-api="getWorkspaceOnline" ref="tableRef" row-key="id">
-      <template #operation="scope">
-        <el-button @click="detail(scope.row)" type="primary" link>查看</el-button>
-      </template>
-    </pro-table>
-
-    <!-- 详情 -->
-    <detail-dialog ref="detailDialogRef" />
+  <div class="card h-full flex justify-center items-center">
+    <div>/src/views{{ $route.path }}/index.vue</div>
   </div>
 </template>
