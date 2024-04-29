314eafdd2c3f8d3d8d7b23d471e4d2cc0380de07
Min <solunar@vip.qq.com>
2024-04-29 15:15:55 +0800

    feat: 整理接口

--- src/api/modules/workspace.ts
+++ src/api/modules/workspace.ts
@@ -31,16 +31,12 @@ export function calcRiskGrade() {
   return http.get<number[]>('/mock/co/workspace/calc-risk-grade');
 }
 
-//
-//
-//
-
-// 获取风险更新记录
+// 风险更新记录
 export function getWorkspaceLog() {
   return http.get('/mock/co/workspace/log');
 }
 
-// 获取风险统计信息
+// 风险统计信息
 export function getWorkspaceStatistics() {
   return http.get('/mock/co/workspace/statistics');
 }
