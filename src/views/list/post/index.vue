5db93c1d2b2409981b6e9e64f54bd5d258cda0ad
Min <solunar@vip.qq.com>
2024-05-06 15:25:43 +0800

    feat: 初始化清单管理

--- src/views/list/post/index.vue
+++ src/views/list/post/index.vue
@@ -1,41 +1,17 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
-import { getWorkspaceOnline } from '@/api/modules/workspace';
-import { ColumnProps } from '@/components/pro-table/interface';
 
-import ProTable from '@/components/pro-table/index.vue';
-import DetailDialog from './dialogs/detail.vue';
+import DutyComponent from './views/duty.vue';
+import TaskComponent from './views/task.vue';
+import OperationComponent from './views/operation.vue';
 
-const tableRef = ref();
-const columns: ColumnProps[] = [
-  { prop: 'placeName', label: '单位名称', search: { el: 'input' } },
-  { prop: 'countyName', label: '行政区划', search: { el: 'input' } },
-  { prop: 'secondName', label: '行业领域' },
-  { prop: 'officeName', label: '行业管理部门' },
-  { prop: 'isSmallName', label: '企业规模' },
-  { prop: 'oot', label: '数据来源' },
-  { prop: 'statusName', label: '账号审核状态' },
-  { prop: 'checkStatusName', label: '核查状态' },
-  { prop: 'checkOfficeName', label: '核查机构' },
-  { prop: 'checkPersonnelName', label: '核查人' },
-  { prop: 'checkTime', label: '核查时间' },
-  { prop: 'allSource', label: '总得分' },
-  { prop: 'operation', label: '操作', width: 100 }
-];
-
-const detailDialogRef = ref();
-const detail = (row: any) => detailDialogRef.value.open(row);
+const active = ref(0);
 </script>
 
 <template>
-  <div class="h-full flex flex-col">
-    <pro-table :columns="columns" :request-api="getWorkspaceOnline" ref="tableRef" row-key="id">
-      <template #operation="scope">
-        <el-button @click="detail(scope.row)" type="primary" link>查看</el-button>
-      </template>
-    </pro-table>
-
-    <!-- 详情 -->
-    <detail-dialog ref="detailDialogRef" />
-  </div>
+  <el-tabs v-model="active" type="border-card" class="h-full tabs-flex no-card">
+    <duty-component :name="0" label="责任清单" lazy />
+    <task-component :name="1" label="任务清单" lazy />
+    <operation-component :name="2" label="操作清单" lazy />
+  </el-tabs>
 </template>
