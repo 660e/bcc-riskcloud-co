d042927ad9526cee50320fd1d1b1db5d8c9ab180
Min <solunar@vip.qq.com>
2024-05-06 14:47:44 +0800

    feat: 主体责任清单mock数据

--- src/views/list/duty/index.vue
+++ src/views/list/duty/index.vue
@@ -1,37 +1,67 @@
-<script lang="ts" setup>
+<script lang="tsx" setup>
 import { ref } from 'vue';
+import { ElMessage, ElMessageBox } from 'element-plus';
 import { getDuty } from '@/api/modules/list';
+import { getDictDataType } from '@/api/modules/system';
+import { deleteItem } from '@/api/modules/company';
 import { ColumnProps } from '@/components/pro-table/interface';
-
 import ProTable from '@/components/pro-table/index.vue';
 import DetailDialog from './dialogs/detail.vue';
 
 const tableRef = ref();
 const columns: ColumnProps[] = [
-  { prop: 'placeName', label: '单位名称', search: { el: 'input' } },
-  { prop: 'countyName', label: '行政区划', search: { el: 'input' } },
-  { prop: 'secondName', label: '行业领域' },
-  { prop: 'officeName', label: '行业管理部门' },
-  { prop: 'isSmallName', label: '企业规模' },
-  { prop: 'oot', label: '数据来源' },
-  { prop: 'statusName', label: '账号审核状态' },
-  { prop: 'checkStatusName', label: '核查状态' },
-  { prop: 'checkOfficeName', label: '核查机构' },
-  { prop: 'checkPersonnelName', label: '核查人' },
-  { prop: 'checkTime', label: '核查时间' },
-  { prop: 'allSource', label: '总得分' },
-  { prop: 'operation', label: '操作', width: 100 }
+  { type: 'selection', width: 0 },
+  { prop: 'id', label: '序号', width: 100 },
+  { prop: 'content', label: '清单内容', search: { el: 'input' } },
+  {
+    prop: 'type',
+    label: '清单类型',
+    width: 150,
+    enum: () => getDictDataType('list_type'),
+    fieldNames: { label: 'dictLabel', value: 'dictValue' },
+    render: scope => {
+      switch (scope.row.type) {
+        case '0':
+          return <el-tag type='primary'>共性清单</el-tag>;
+        case '1':
+          return <el-tag type='success'>个性清单</el-tag>;
+      }
+    }
+  },
+  { prop: 'operation', label: '操作', width: 150 }
 ];
+
+const createDeptDialogRef = ref();
+const create = (row: any = {}) => createDeptDialogRef.value.open(row);
 
 const detailDialogRef = ref();
 const detail = (row: any) => detailDialogRef.value.open(row);
+
+const remove = (row: any) => {
+  const name = row.id ? `“${row.id}”` : '';
+  const ids = row.id ? [row.id] : tableRef.value.selectedListIds;
+  ElMessageBox.confirm(`是否删除${name}？`, '系统提示', { type: 'warning' })
+    .then(async () => {
+      const { msg } = await deleteItem(ids.join(','));
+      tableRef.value.search(tableRef.value.pageable?.pageNum);
+      tableRef.value.clearSelection();
+      ElMessage.success(msg);
+    })
+    .catch(() => false);
+};
 </script>
 
 <template>
   <div class="h-full flex flex-col">
     <pro-table :columns="columns" :request-api="getDuty" ref="tableRef" row-key="id">
+      <template #tableHeader>
+        <el-button @click="create" type="primary">新增共性清单</el-button>
+        <el-button @click="create" type="primary">新增个性清单</el-button>
+        <el-button :disabled="!tableRef?.selectedListIds.length" @click="remove" type="danger" plain>删除</el-button>
+      </template>
       <template #operation="scope">
-        <el-button @click="detail(scope.row)" type="primary" link>查看</el-button>
+        <el-button v-if="scope.row.type === '1'" @click="detail(scope.row)" type="primary" link>编辑</el-button>
+        <el-button @click="remove(scope.row)" type="danger" link>删除</el-button>
       </template>
     </pro-table>
 
