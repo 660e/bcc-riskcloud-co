0245658d9d5b631e3f8e7b823bdd38d58aaba9d9
Min <solunar@vip.qq.com>
2024-05-24 16:30:19 +0800

    feat: 应急能力评估

--- src/views/workspace/risk/views/step-5/index.vue
+++ src/views/workspace/risk/views/step-5/index.vue
@@ -52,7 +52,10 @@ const typeChange = (value: string, tr: any) => {
 <template>
   <div class="card flex-1 flex flex-col">
     <div class="flex items-center space-x-5 p-2.5">
-      <el-button>导出</el-button>
+      <div>
+        <el-button type="primary">暂存</el-button>
+        <el-button>导出</el-button>
+      </div>
       <div class="flex items-center">
         应得分：<span class="text-xl">{{ par }}</span>
       </div>
