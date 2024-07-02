94fbfae120bf3a93b1710ecf57cb48a85d2fbd77
Min <solunar@vip.qq.com>
2024-07-02 09:59:27 +0800

    feat: 调整首页模块位置

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,12 +7,12 @@ import { CircleCheck, Clock, DocumentDelete, Histogram } from '@element-plus/ico
 import QRCode from 'qrcode';
 
 const icons: any = {
-  dangerSummary: [CircleCheck, Clock, DocumentDelete],
-  riskSummary: [Histogram, ...new Array(4).fill(new URL('../../assets/icons/home/warning-1.png', import.meta.url).href)]
+  riskSummary: [Histogram, ...new Array(4).fill(new URL('../../assets/icons/home/warning-1.png', import.meta.url).href)],
+  dangerSummary: [CircleCheck, Clock, DocumentDelete]
 };
 
-const dangerSummary = ref();
 const riskSummary = ref();
+const dangerSummary = ref();
 const tasksData = ref();
 const helpData = ref();
 const tableWrapperRef = ref();
@@ -25,12 +25,6 @@ const backgroundImage = (index: number) => {
 onMounted(async () => {
   QRCode.toCanvas(document.getElementById('qrcode'), (await companyApi.qrcode('1')).data, { margin: 0, width: 120 });
 
-  const reformStatusDict = (await systemApi.dict('reform_status')).data;
-  dangerSummary.value = (await dangerApi.summary()).data.map((item: any) => {
-    item.label = reformStatusDict.find(dict => dict.dictValue === item.code)?.dictLabel || '-';
-    return item;
-  });
-
   const riskLevelDict = (await systemApi.dict('risk_level')).data;
   riskSummary.value = (await riskApi.summary()).data.map((item: any) => {
     item.label = riskLevelDict.find(dict => dict.dictValue === item.code)?.dictLabel || '-';
@@ -40,6 +34,12 @@ onMounted(async () => {
     label: '全部',
     value: riskSummary.value.reduce((a: number, c: any) => a + c.value, 0),
     increase: riskSummary.value.reduce((a: number, c: any) => a + c.increase, 0)
+  });
+
+  const reformStatusDict = (await systemApi.dict('reform_status')).data;
+  dangerSummary.value = (await dangerApi.summary()).data.map((item: any) => {
+    item.label = reformStatusDict.find(dict => dict.dictValue === item.code)?.dictLabel || '-';
+    return item;
   });
 
   await nextTick();
@@ -53,6 +53,25 @@ onMounted(async () => {
 
 <template>
   <div class="h-full flex flex-col space-y-2.5">
+    <div class="card p-5 space-y-5">
+      <div class="c-subtitle-1">风险信息</div>
+      <div class="flex">
+        <div v-for="(item, index) in riskSummary" :key="index" class="flex-1 flex items-center space-x-5">
+          <el-icon :style="{ backgroundColor: COLORS.risk_level[index] }" class="text-5xl rounded p-2 text-white">
+            <component v-if="index === 0" :is="icons.riskSummary[index]" />
+            <img v-else :src="icons.riskSummary[index]" />
+          </el-icon>
+          <div class="space-y-1">
+            <div>{{ item.label }}</div>
+            <div class="space-x-5">
+              <b :style="{ color: COLORS.risk_level[index] }">{{ item.value }}</b>
+              <span>较上月{{ item.increase < 0 ? item.increase : `+${item.increase}` }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="card p-5 flex">
       <div class="space-y-5 flex-1">
         <div class="c-subtitle-1">企业总码</div>
@@ -74,25 +93,6 @@ onMounted(async () => {
                 <b class="text-xl">{{ item.value }}条</b>
                 <span>较上月{{ item.increase < 0 ? item.increase : `+${item.increase}` }}</span>
               </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="card p-5 space-y-5">
-      <div class="c-subtitle-1">风险信息</div>
-      <div class="flex">
-        <div v-for="(item, index) in riskSummary" :key="index" class="flex-1 flex items-center space-x-5">
-          <el-icon :style="{ backgroundColor: COLORS.risk_level[index] }" class="text-5xl rounded p-2 text-white">
-            <component v-if="index === 0" :is="icons.riskSummary[index]" />
-            <img v-else :src="icons.riskSummary[index]" />
-          </el-icon>
-          <div class="space-y-1">
-            <div>{{ item.label }}</div>
-            <div class="space-x-5">
-              <b :style="{ color: COLORS.risk_level[index] }">{{ item.value }}</b>
-              <span>较上月{{ item.increase < 0 ? item.increase : `+${item.increase}` }}</span>
             </div>
           </div>
         </div>
